18f5c907020a6e62d43da93a3e1db5ef60654915
aakansha1216@gmail.com <aakansha1216@gmail.com>
2024-12-10 06:52:53 +0000

    Update Python_Bitbucket_Pipeline to use new prophecy-libs version and refactor Subgraph_1 configuration handling

--- pipelines/Python_Bitbucket_Pipeline/code/python_bitbucket_pipeline/graph/Subgraph_1/Subgraph_1.py
+++ pipelines/Python_Bitbucket_Pipeline/code/python_bitbucket_pipeline/graph/Subgraph_1/Subgraph_1.py
@@ -2,12 +2,14 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from prophecy.utils import *
+from python_bitbucket_pipeline.udfs.UDFs import *
 from . import *
 from .config import *
 
-def Subgraph_1(spark: SparkSession, config: SubgraphConfig, in0: DataFrame) -> (DataFrame, DataFrame):
-    Config.update(config)
+def Subgraph_1(spark: SparkSession, subgraph_config: SubgraphConfig, in0: DataFrame) -> (DataFrame, DataFrame):
+    Config.update(subgraph_config)
     df_Reformat_2 = Reformat_2(spark, in0)
     df_CustomReformatBitbucket_2 = CustomReformatBitbucket_2(spark, df_Reformat_2)
+    subgraph_config.update(Config)
 
     return df_Reformat_2, df_CustomReformatBitbucket_2
